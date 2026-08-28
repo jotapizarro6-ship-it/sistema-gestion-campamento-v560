b4a2d19b5bdf41172194269162b8b85c52f479e7
jotapizarro6-ship-it <jotapizarro6@gmail.com>
2026-08-27 21:08:06 -0400

    Corregir carga de capa responsiva

--- assets/app-4.js
+++ assets/app-4.js
@@ -1,6 +1,6 @@
 'use strict';
 // Carga síncrona del núcleo existente y luego la capa responsiva. En los smoke tests document.write no existe, por lo que no se altera el entorno de pruebas.
 if(typeof document!=='undefined'&&typeof document.write==='function'){
-  document.write('<script src="assets/app-4-core.js"><\\/script>');
-  document.write('<script src="assets/responsive-admin.js"><\\/script>');
+  document.write('<script src="assets/app-4-core.js"></script>');
+  document.write('<script src="assets/responsive-admin.js"></script>');
 }
